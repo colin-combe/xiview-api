1990b2c18cdc9b1eef8c1c514153edd5982c98c7
colin-combe <colin.combe@ed.ac.uk>
2020-04-12 10:56:43 +0100

    allow mssed up line endings in FASTA files

--- SimpleFASTA.py
+++ SimpleFASTA.py
@@ -6,27 +6,26 @@ def get_db_sequence_dict(fasta_file_list):
     sequence = ""
     description = None
     for fasta_file in fasta_file_list:
-        with open(fasta_file) as f:
-            for line in f:
-                # semi-colons indicate comments, ignore them
-                if not line.startswith(";"):
-                    if line.startswith(">"):
-                        if identifier is not None:
-                            add_entry(identifier, sequence, description, db_sequence_dict)
+        for line in open(fasta_file).read().splitlines():
+            # semi-colons indicate comments, ignore them
+            if not line.startswith(";"):
+                if line.startswith(">"):
+                    if identifier is not None:
+                        add_entry(identifier, sequence, description, db_sequence_dict)
 
-                            #clear sequence
-                            sequence = ""
+                        #clear sequence
+                        sequence = ""
 
-                        # get new identifier
-                        identifier = line
-                        if " " not in line:
-                            identifier = line[1:].rstrip()
-                        else:
-                            iFirstSpace = line.index(" ")
-                            identifier = line[1:iFirstSpace].rstrip()
-                            description = line[iFirstSpace:].rstrip()
+                    # get new identifier
+                    identifier = line
+                    if " " not in line:
+                        identifier = line[1:].rstrip()
                     else:
-                        sequence += line.rstrip()
+                        iFirstSpace = line.index(" ")
+                        identifier = line[1:iFirstSpace].rstrip()
+                        description = line[iFirstSpace:].rstrip()
+                else:
+                    sequence += line.rstrip()
 
     # add last entry
     if identifier is not None:
