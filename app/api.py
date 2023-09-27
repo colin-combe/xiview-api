be98412eeffb25e557cdcca8fe7cf9eee282e280
sureshhewabi <sureshhewabi@gmail.com>
2023-09-27 13:17:18 +0100

    add prefix URL

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 
 from app.routes.main import main_router
 
-app = FastAPI(openapi_url="/api/openapi.json", docs_url="/api/docs")
+app = FastAPI(openapi_url="/xi-converter/api/openapi.json", docs_url="/xi-converter/api/docs")
 
 
-app.include_router(main_router, prefix="/api")
+app.include_router(main_router, prefix="/xi-converter")
