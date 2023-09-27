7eeb745e9834e1035a9816cf0ede4e21318033c1
sureshhewabi <sureshhewabi@gmail.com>
2023-09-27 11:19:41 +0100

    add prefix URL

--- app/routes/main.py
+++ app/routes/main.py
@@ -4,7 +4,8 @@ from app.routes.index import get_session
 from process_dataset import convert_pxd_accession
 import os
 
-main_router = APIRouter(prefix="/pride/archive/xi-mzidentml-converter-api")
+# main_router = APIRouter(prefix="/pride/archive/xi-mzidentml-converter-api")
+main_router = APIRouter(prefix="/api")
 
 
 @main_router.get("/parse/{px_accession}", tags=["Main"])
