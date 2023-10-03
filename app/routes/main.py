a133729ea7147bf70303d0ea392b742c2acc0083
sureshhewabi <sureshhewabi@gmail.com>
2023-10-03 13:53:35 +0100

    Image name change

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,4 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from app.routes.index import get_session
+from fastapi import APIRouter
 from process_dataset import convert_pxd_accession_from_pride
 import os
 
