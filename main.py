52b81245b606eb2b4e1db880449018be0d26b9de
sureshhewa <sureshhewabi@gmail.com>
2023-11-21 15:16:35 +0000

    Correct port

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uvicorn
 
+from app.api import app
+
 if __name__ == "__main__":
-    uvicorn.run("app.api:app",
+    uvicorn.run(app,
                 host="0.0.0.0",
-                port=6000,
+                port=3000,
                 reload=True,
                 log_level="info",  # Set the desired log level ("debug", "info", "warning", etc.)
                 log_config="logging.ini"  # Specify the path to your logging configuration file
