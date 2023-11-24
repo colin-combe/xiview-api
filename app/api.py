c44c07e6f1289d307fbb55d9a8f29578008f1b5f
sureshhewa <sureshhewabi@gmail.com>
2023-11-24 09:35:02 +0000

    change the URL

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 
 from app.routes.pride import pride_router
 
-app = FastAPI(openapi_url="/pride/archive/xiview/xi-converter/api/openapi.json", docs_url="/pride/archive/xiview/xi-converter/api/docs")
+app = FastAPI(openapi_url="/pride/archive/xiview/ws/api/openapi.json", docs_url="/pride/archive/xiview/ws/api/docs")
 
 
-app.include_router(pride_router, prefix="/pride/archive/xiview/xi-converter")
+app.include_router(pride_router, prefix="/pride/archive/xiview/ws/")
