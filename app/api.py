2220b1b7249b05c7d58ab8ddba3436c8cfc3bcbb
sureshhewa <sureshhewabi@gmail.com>
2023-11-24 11:12:23 +0000

    URL bug fix

--- app/api.py
+++ app/api.py
@@ -5,4 +5,4 @@ from app.routes.pride import pride_router
 app = FastAPI(openapi_url="/pride/archive/xiview/ws/api/openapi.json", docs_url="/pride/archive/xiview/ws/api/docs")
 
 
-app.include_router(pride_router, prefix="/pride/archive/xiview/ws/")
+app.include_router(pride_router, prefix="/pride/archive/xiview/ws")
