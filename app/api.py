139ce19d76c62592f3070a68110855b5aa23096b
sureshhewa <sureshhewabi@gmail.com>
2024-01-31 17:24:09 +0000

    changed URL - Important change!

--- app/api.py
+++ app/api.py
@@ -13,8 +13,8 @@ app = FastAPI(title="xi-mzidentml-converter WS",
                   "name": "Apache 2.0",
                   "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
               },
-              openapi_url="/pride/archive/crosslinking/ws/openapi.json",
-              docs_url="/pride/archive/crosslinking/ws/docs")
+              openapi_url=" /pride/ws/archive/crosslinking/openapi.json",
+              docs_url="/pride/ws/archive/crosslinking/docs")
 
 # Set up CORS middleware
 origins = ["*"]  # Update this with your allowed origins
@@ -27,8 +27,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(pride_router, prefix="/pride/archive/crosslinking/ws")
-app.include_router(pdbdev_router, prefix="/pride/archive/crosslinking/ws/pdbdev")
-app.include_router(xiview_data_router, prefix="/pride/archive/crosslinking/ws/data")
-app.include_router(parser_router, prefix="/pride/archive/xiview/crosslinking/parse")
+app.include_router(pride_router, prefix="/pride/ws/archive/crosslinking")
+app.include_router(pdbdev_router, prefix="/pride/ws/archive/crosslinking/pdbdev")
+app.include_router(xiview_data_router, prefix="/pride/ws/archive/crosslinking/data")
+app.include_router(parser_router, prefix="/pride/ws/archive/crosslinking/parse")
 
