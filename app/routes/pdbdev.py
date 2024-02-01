9d23fb14ee04a891e1071bfe983098d9b7a5797a
sureshhewa <sureshhewabi@gmail.com>
2024-02-01 11:48:41 +0000

    add accession numbers to PDB-Dev endpoint output

--- app/routes/pdbdev.py
+++ app/routes/pdbdev.py
@@ -102,14 +102,16 @@ async def get_psm_level_residue_pairs(project_id, passing_threshold):
         conn = await get_db_connection()
         cur = conn.cursor(cursor_factory=RealDictCursor)
 
-        sql = """SELECT si.id, u.identification_file_name as file, si.pass_threshold as pass,
-pe1.dbsequence_ref as prot1, (pe1.pep_start + mp1.link_site1 - 1) as pos1,
-pe2.dbsequence_ref as prot2, (pe2.pep_start + mp2.link_site1 - 1) as pos2
+        sql = """SELECT dbseq.id, u.identification_file_name, dbseq.sequence, dbseq.accession
+pe1.dbsequence_ref as prot1, dbs1.accession as prot1_acc, (pe1.pep_start + mp1.link_site1 - 1) as pos1,
+pe2.dbsequence_ref as prot2, dbs2.accession as prot2_acc, (pe2.pep_start + mp2.link_site1 - 1) as pos2
 FROM spectrumidentification si INNER JOIN
 modifiedpeptide mp1 ON si.pep1_id = mp1.id AND si.upload_id = mp1.upload_id INNER JOIN
 peptideevidence pe1 ON mp1.id = pe1.peptide_ref AND mp1.upload_id = pe1.upload_id INNER JOIN
+dbsequence dbs1 ON pe1.dbsequence_ref = dbs1.id AND pe1.upload_id = dbs1.upload_id INNER JOIN
 modifiedpeptide mp2 ON si.pep2_id = mp2.id AND si.upload_id = mp2.upload_id INNER JOIN
 peptideevidence pe2 ON mp2.id = pe2.peptide_ref AND mp2.upload_id = pe2.upload_id INNER JOIN
+dbsequence dbs2 ON pe2.dbsequence_ref = dbs2.id AND pe2.upload_id = dbs2.upload_id INNER JOIN
 upload u on u.id = si.upload_id
 where u.id = ANY (%s) and mp1.link_site1 > 0 and mp2.link_site1 > 0 AND pe1.is_decoy = false AND pe2.is_decoy = false
 """
