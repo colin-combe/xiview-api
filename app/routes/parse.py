852072341a83ab4c95c84dc9012cc7b512b21c2a
sureshhewa <sureshhewabi@gmail.com>
2024-02-05 09:41:17 +0000

    fix bug

--- app/routes/parse.py
+++ app/routes/parse.py
@@ -2,7 +2,7 @@ import base64
 import configparser
 import traceback
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi import HTTPException, Security
 from fastapi.params import Body
 from fastapi.security import APIKeyHeader
@@ -25,28 +25,30 @@ parser_router = APIRouter()
 config = configparser.ConfigParser()
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
+
 class TableNamesEnum(str, Enum):
     """Enumeration of Table names"""
     analysiscollection = "analysiscollection"
-    dbsequence ="dbsequence"
+    dbsequence = "dbsequence"
     enzyme = "enzyme"
-    modifiedpeptide ="modifiedpeptide"
-    peptideevidence ="peptideevidence"
-    searchmodification ="searchmodification"
-    spectrum ="spectrum"
-    spectrumidentification ="spectrumidentification"
-    spectrumidentificationprotocol ="spectrumidentificationprotocol"
+    modifiedpeptide = "modifiedpeptide"
+    peptideevidence = "peptideevidence"
+    searchmodification = "searchmodification"
+    spectrum = "spectrum"
+    spectrumidentification = "spectrumidentification"
+    spectrumidentificationprotocol = "spectrumidentificationprotocol"
     upload = "upload"
     projectdetail = "projectdetail"
     projectsubdetail = "projectsubdetail"
 
+
 @parser_router.post("/write_data", tags=["Parser"])
 async def write_data(
         table: TableNamesEnum = Body(..., description="table name", embeded=True),
-        data = Body(..., description="table data", embeded=True),
+        data=Body(..., description="table data", embeded=True),
         api_key: str = Security(get_api_key),
         session: Session = Depends(get_session)):
-        """
+    """
         Insert data into table.
 
         :param session:
@@ -54,30 +56,31 @@ async def write_data(
         :param table: (str) Table name
         :param data: (list dict) data to insert.
         """
-        try:
-            if table==TableNamesEnum.spectrum:
-                print("spectrum table")
-                i=0
-                for spectra in data:
-                    spectra["mz"] =  base64.b64decode(spectra["mz"])
-                    spectra["intensity"] =  base64.b64decode(spectra["intensity"])
-                    data[i] = spectra
-                    i+=1
+    try:
+        if table == TableNamesEnum.spectrum:
+            print("spectrum table")
+            i = 0
+            for spectra in data:
+                spectra["mz"] = base64.b64decode(spectra["mz"])
+                spectra["intensity"] = base64.b64decode(spectra["intensity"])
+                data[i] = spectra
+                i += 1
 
-            table = Table(table, meta, autoload_with=engine)
-            with engine.connect() as conn:
-                statement = table.insert().values(data)
-                result = conn.execute(statement)
-                conn.commit()
-                conn.close()
-            return result
-
-        except Exception as e:
-            print(f"Caught an exception: {e}")
-            traceback.print_exc()
-        finally:
-            session.close()
+        table = Table(table, meta, autoload_with=engine)
+        with engine.connect() as conn:
+            statement = table.insert().values(data)
+            result = conn.execute(statement)
+            conn.commit()
+            conn.close()
         return result
+
+    except Exception as e:
+        print(f"Caught an exception: {e}")
+        traceback.print_exc()
+    finally:
+        session.close()
+    return result
+
 
 @parser_router.post("/write_new_upload", tags=["Parser"])
 def write_new_upload(
@@ -95,6 +98,7 @@ def write_new_upload(
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
+
 @parser_router.post("/write_mzid_info", tags=["Admin"])
 def write_mzid_info(analysis_software_list=Body(..., embeded=True),
                     spectra_formats=Body(..., embeded=True),
@@ -102,7 +106,7 @@ def write_mzid_info(analysis_software_list=Body(..., embeded=True),
                     audits=Body(..., embeded=True),
                     samples=Body(..., embeded=True),
                     bib=Body(..., embeded=True),
-                    upload_id:int=None,
+                    upload_id: int = Query(default=None),
                     api_key: str = Security(get_api_key),
                     session: Session = Depends(get_session)):
     """
@@ -120,21 +124,22 @@ def write_mzid_info(analysis_software_list=Body(..., embeded=True),
     """
     upload = Table("upload", meta, autoload_with=engine, quote=False)
     stmt = upload.update().where(upload.c.id == str(upload_id)).values(
-            analysis_software_list=analysis_software_list,
-            spectra_formats=spectra_formats,
-            provider=provider,
-            audit_collection=audits,
-            analysis_sample_collection=samples,
-            bib=bib
-        )
+        analysis_software_list=analysis_software_list,
+        spectra_formats=spectra_formats,
+        provider=provider,
+        audit_collection=audits,
+        analysis_sample_collection=samples,
+        bib=bib
+    )
     with engine.connect() as conn:
         conn.execute(stmt)
         conn.commit()
 
+
 @parser_router.post("/write_other_info", tags=["Parser"])
-def write_other_info(contains_crosslinks = Body(..., description="contains_crosslinks", embeded=True),
-                     upload_warnings = Body(..., description="upload_warnings", embeded=True),
-                     upload_id:int=None,
+def write_other_info(contains_crosslinks=Body(..., description="contains_crosslinks", embeded=True),
+                     upload_warnings=Body(..., description="upload_warnings", embeded=True),
+                     upload_id: int = None,
                      api_key: str = Security(get_api_key),
                      session: Session = Depends(get_session)):
     """
@@ -151,8 +156,8 @@ def write_other_info(contains_crosslinks = Body(..., description="contains_cross
     upload = Table("upload", meta, autoload_with=engine, quote=False)
     with engine.connect() as conn:
         stmt = upload.update().where(upload.c.id == str(upload_id)).values(
-                contains_crosslinks=contains_crosslinks,
-                upload_warnings=upload_warnings,
-            )
+            contains_crosslinks=contains_crosslinks,
+            upload_warnings=upload_warnings,
+        )
         conn.execute(stmt)
         conn.commit()
