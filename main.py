fa2d174a35d5f5184e78e2a0e0dba721c6b79c0a
sureshhewa <sureshhewabi@gmail.com>
2024-05-31 11:59:15 +0100

    increase timeout to 5 min

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
                 port=3000,
                 log_level="info",  # Set the desired log level ("debug", "info", "warning", etc.)
                 log_config="logging.ini",  # Specify the path to your logging configuration file
-                timeout_keep_alive=120,  # Keep-alive timeout in seconds
+                timeout_keep_alive=300,  # Keep-alive timeout in seconds
                 )
